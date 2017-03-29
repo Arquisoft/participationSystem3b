7e5a389ecd12007febfa6f5c5539b7448d265920
Moises Muñiz <uo237351@uniovi.es>
2017-03-29 10:31:22 +0200

    Login + ¿Me voy enterando?

--- src/main/java/es/uniovi/asw/webService/MainController.java
+++ src/main/java/es/uniovi/asw/webService/MainController.java
@@ -1,15 +1,21 @@
 package es.uniovi.asw.webService;
 
 
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import es.uniovi.asw.producers.KafkaProducer;
 
 @Controller
+@Scope("session")
 public class MainController {
 	
 	 @Autowired
@@ -26,5 +32,20 @@ public class MainController {
         kafkaProducer.send("exampleTopic", message.getMessage());
         return "redirect:/";
     }
+    
+    @RequestMapping(value = "/login", method = RequestMethod.POST)
+    public String login(HttpSession session,Model model,@RequestParam String username, @RequestParam String password) {
+    	
+    	if(userValidator.validate(username, password,"citi")){
+    		session.setAttribute("user", new User(username,password));
+    		return "user";
+    	}
+    	
+    	else if(userValidator.validate(username, password,"admin")){
+    		session.setAttribute("user", new User(username,password));
+    		return "admin";
+    	}
 
+    		return "login";	
+    }
 }
---
+++ src/main/java/es/uniovi/asw/webService/userValidator.java
@@ -0,0 +1,7 @@
+package es.uniovi.asw.webService;
+
+public class userValidator {
+	static boolean validate(String name,String passwd,String type){
+		return true;
+	}
+}
