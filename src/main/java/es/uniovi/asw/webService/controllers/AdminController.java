5ea119255f9655535ea73d4fd158229c932fd4d8
Moises Muñiz <uo237351@uniovi.es>
2017-04-05 11:22:46 +0200

    Administracion Completa

--- src/main/java/es/uniovi/asw/webService/controllers/AdminController.java
+++ src/main/java/es/uniovi/asw/webService/controllers/AdminController.java
@@ -1,5 +1,11 @@
 package es.uniovi.asw.webService.controllers;
 
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+
 import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -24,9 +30,82 @@ public class AdminController {
 	
 	@RequestMapping(value = "/crearCategoria", method = RequestMethod.POST)
     public String CrearSolicitud(HttpSession session,Model model,@RequestParam String nombre,@RequestParam String fechamin,@RequestParam String fechamax,@RequestParam String numVotos,@RequestParam String denegadas) throws BusinessException {
-    		Categoria categoria = new Categoria(nombre,null,null,Integer.valueOf(numVotos));
+			DateFormat format = new SimpleDateFormat("dd/MM/yyy");
+			Date fechaMi=new Date();
+			Date fechaMa=new Date();
+			try {
+				fechaMi = format.parse(fechamin);
+				fechaMa = format.parse(fechamax);
+			} catch (ParseException e) {
+				e.printStackTrace();
+				return "listaSolicitudesadmin";
+			}
+			int numV=Integer.parseInt(numVotos);
+			Categoria categoria=new Categoria(nombre,fechaMi,fechaMa,numV);
+			ArrayList<String> paldeneg=new ArrayList<String>();
+			for(String pal:denegadas.split(";")){
+				paldeneg.add(pal);
+			}
+			categoria.setPalabrasNoPermitidas(paldeneg);
 			Services.getAdminServices().addCategoria(categoria);
 			Actions.listarSugerencias(model, null);
     		return "listaSolicitudesadmin";
     }
+	
+	@RequestMapping(value = "/eliminar", method = RequestMethod.POST)
+    public String CrearSolicitud(HttpSession session,Model model,@RequestParam("sugerencia") String id) throws BusinessException {
+			Services.getAdminServices().deleteSugerencia(Long.parseLong(id));
+			Actions.listarSugerencias(model, null);
+    		return "listaSolicitudesadmin";
+    }
+	
+	@RequestMapping(value = "/eliminarCategoria", method = RequestMethod.POST)
+    public String EliminarCategoria(HttpSession session,Model model,@RequestParam("categoria")  Long  id) throws BusinessException {
+			Services.getAdminServices().deleteCategoria(id);
+			Actions.listarSugerencias(model, null);
+    		return "listaSolicitudesadmin";
+    }
+	
+	@RequestMapping(value = "/modificarCat", method = RequestMethod.POST)
+    public String IrAModificarCat(HttpSession session,Model model,@RequestParam("categoria") Long cat) throws BusinessException {
+			Categoria categor=Services.getSystemServices().findCategoriaById(cat);
+			model.addAttribute("cat", categor);
+			String palabras="";
+			for(String pal:categor.getPalabrasNoPermitidas()){
+				palabras+=pal+";";
+			}
+			model.addAttribute("palabrasNoPermitidas", palabras);
+			DateFormat format = new SimpleDateFormat("dd/MM/yyy");
+			String fin = format.format(categor.getFechaFin());
+			String inicio = format.format(categor.getFechaInicio());
+			model.addAttribute("fechaInicio", inicio);
+			model.addAttribute("fechaFin", fin);
+			
+    		return "editarCategoria";
+    }
+	
+	@RequestMapping(value = "/modificarCategoria", method = RequestMethod.POST)
+    public String ModificarCategoria(HttpSession session,Model model,@RequestParam String nombre,@RequestParam String fechamin,@RequestParam String fechamax,@RequestParam String numVotos,@RequestParam String denegadas) throws BusinessException {
+			DateFormat format = new SimpleDateFormat("dd/MM/yyy");
+			Date fechaMi=new Date();
+			Date fechaMa=new Date();
+			try {
+				fechaMi = format.parse(fechamin);
+				fechaMa = format.parse(fechamax);
+			} catch (ParseException e) {
+				e.printStackTrace();
+				return "listaSolicitudesadmin";
+			}
+			int numV=Integer.parseInt(numVotos);
+			Categoria categoria=new Categoria(nombre,fechaMi,fechaMa,numV);
+			ArrayList<String> paldeneg=new ArrayList<String>();
+			for(String pal:denegadas.split(";")){
+				paldeneg.add(pal);
+			}
+			categoria.setPalabrasNoPermitidas(paldeneg);
+			Services.getAdminServices().updateCategoria(categoria);
+			Actions.listarSugerencias(model, null);
+    		return "listaSolicitudesadmin";
+    }
+	
 }
